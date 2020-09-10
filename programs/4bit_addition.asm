e91950291874a50f8d4947044ccfe96d2fce2f12
Konstanty <kostu96@gmail.com>
2020-09-10 14:58:49 +0200

    Separate emulator and assembler parts

---
+++ programs/4bit_addition.asm
@@ -0,0 +1,9 @@
+; add two 4bit numbers on the Intel 4004
+;
+    FIM P0, $A2	; initialize: R0=2 R1=A
+    LD R0       ; load R0 into accumulator
+    ADD R1      ; add R1 into accumulator
+    XCH R1      ; and store in R1
+
+DONE
+    JUN DONE    ; endless loop as end of program
