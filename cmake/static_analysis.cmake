53d6ae97e01a7f7cc4c4c2beb36231702c3675dc
Kostu96 <kostu96@gmail.com>
2021-08-04 13:16:35 +0200

    Test clang-tidy

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -5,9 +5,11 @@ if (CMAKE_CXX_CLANG_TIDY)
     set(CLANG_TIDY_CONFIG
         "--checks=\
             clang-analiser-*,\
+            clang-diagnostic-*\
             bugprone-*,\
             cert-*,\
-            modernize-*"
+            modernize-*,\
+            performance-*"
     )
     
     list(APPEND CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_CONFIG})
