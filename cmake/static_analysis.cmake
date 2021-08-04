7c4cbefe69f2c7eb28c27655485d8cdcfa94381c
Kostu96 <kostu96@gmail.com>
2021-08-04 12:36:39 +0200

    Test clang-tidy

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -4,9 +4,10 @@ find_program(CMAKE_CXX_CLANG_TIDY NAMES clang-tidy)
 if (CMAKE_CXX_CLANG_TIDY)
     set(CLANG_TIDY_CONFIG
         "--checks=\
+            clang-analiser-*,\
             bugprone-*,\
             cert-*,\
-            cppcoreguidelines-*,-cppcoreguidelines-avoid-magic-numbers,-cppcoreguidelines-pro-bounds-constant-array-index"
+            hicpp-*"
     )
     
     list(APPEND CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_CONFIG})
