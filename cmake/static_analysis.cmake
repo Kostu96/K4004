f5f51d804a1ec13d1c7a1f4b6896d6b1df12885b
Kostu96 <kostu96@gmail.com>
2021-08-04 12:06:53 +0200

    Test clang-tidy

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -1,13 +1,10 @@
 cmake_minimum_required(VERSION 3.18)
 
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-set(CLANG_TIDY_CONFIG
-    "--checks=*"
-	"--dump-config"
-)
-
 find_program(CMAKE_CXX_CLANG_TIDY NAMES clang-tidy)
 if (CMAKE_CXX_CLANG_TIDY)
+    set(CLANG_TIDY_CONFIG
+        "--checks=*"
+    )
+    
     list(APPEND CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_CONFIG})
 endif()
